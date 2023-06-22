de301eb4e7e5369cb6842e8748e3e862619ef78e
VitalyBoro <127157980+VitalyBoro@users.noreply.github.com>
2023-06-22 19:17:26 +0300

    Update User.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -84,12 +84,3 @@ public class User {
               '}';
    }
 }
-
-   public String getEmail() {
-      return email;
-   }
-
-   public void setEmail(String email) {
-      this.email = email;
-   }
-}
